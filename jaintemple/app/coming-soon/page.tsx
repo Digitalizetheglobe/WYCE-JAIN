2437738e2387f5c971e2e960230283ee37cc888e
Adarshswagh <waghmareadarsh1432@gmail.com>
2026-04-04 15:02:38 +0530

    image update

--- jaintemple/app/coming-soon/page.tsx
+++ jaintemple/app/coming-soon/page.tsx
@@ -8,7 +8,7 @@ export default function ComingSoon() {
       {/* Large Desktop Banner (3780 x 1890) */}
       <div className="hidden lg:block relative w-full h-screen animate-fadeIn">
         <Image
-          src="/home/Jain%20Mandir%20website%20banner%20(1920%20x%201080%20px)%20(3780%20x%201890%20px).png"
+          src="/home/laptop-banner.png"
           alt="Jain Mandir Event Main Banner Large"
           fill
           className="object-cover"
@@ -19,7 +19,7 @@ export default function ComingSoon() {
       {/* Standard Desktop/Tablet Banner (1920 x 1080) */}
       <div className="hidden md:block lg:hidden relative w-full h-screen animate-fadeIn">
         <Image
-          src="/home/Jain%20Mandir%20website%20banner%20(1920%20x%201080%20px).png"
+          src="/home/tab-banner.png"
           alt="Jain Mandir Event Main Banner Standard"
           fill
           className="object-cover"
@@ -30,7 +30,7 @@ export default function ComingSoon() {
       {/* Mobile Banner (1080 x 1920) */}
       <div className="block md:hidden relative w-full h-screen animate-fadeIn">
         <Image
-          src="/home/Jain%20Mandir%20website%20banner%20(1920%20x%201080%20px)%20(1080%20x%201920%20px).png"
+          src="/home/mobile-banner.png"
           alt="Jain Mandir Event Main Banner Mobile"
           fill
           className="object-contain"
